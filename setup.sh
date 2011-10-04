b5652e05f8f115e981e9a50817a3774cb4aca6ad
Aleksey Maksimov <ama385@maersk.com>
2011-10-04 18:08:51 +0200

    Removed cloning of dotfiles

--- setup.sh
+++ setup.sh
@@ -2,7 +2,6 @@
 
 git clone http://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
 git clone https://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
-git clone http://github.com/lechat/dotfiles.git ~/dotfiles
 
 ln -s ~/dotfiles/.vimrc ~/.vimrc
 ln -s ~/dotfiles/.vim ~/.vim
