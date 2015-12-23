a91cfc797de868d873c2e8e5d2be1027dd4f7087
Aleksey Maksimov <ctpeko3a@gmail.com>
2015-12-23 10:24:12 +0800

    Added .gitconfig and rewrote setup.sh

--- setup.sh
+++ setup.sh
@@ -1,44 +1,48 @@
 #!/bin/bash
 
-if [ -e ~/.oh-my-zsh ]
-then
-    mv ~/.oh-my-zsh ~/.oh-my-zsh.old
-fi
+set -o nounset
+set -0 pipefail
+set -0 errexit
 
-if [ -e ~/.vim ]
-then
-    mv ~/.vim ~/.vim.old
-fi
+function save_to_old() {
+    local item=$1
+    if [ -e ~/$item ]; then
+        mv ~/$item ~/$item.old
+    fi
+}
 
-if [ -e ~/.zshrc ]
-then
-    mv ~/.zshrc ~/.zshrc.old
-fi
+function do_clone() {
+    git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
+    git clone https://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
+    git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+}
 
-if [ -e ~/.vimrc ]
-then
-    mv ~/.vimrc ~/.vimrc.old
-fi
+function make_links() {
+    ln -s ~/dotfiles/.vimrc ~/.vimrc
+    ln -s ~/dotfiles/.vim ~/.vim
+    ln -s ~/dotfiles/.zshrc ~/.zshrc
+    ln -s ~/dotfiles/.autoenv ~/.autoenv
+    ln -s ~/dotfiles/.tmux.conf ~/.tmux.conf
+    ln -s ~/dotfiles/.xinitrc ~/.xinitrc
+    ln -s ~/dotfiles/.Xmodmap ~/.Xmodmap
+    ln -s ~/dotfiles/.gitconfig ~/.gitconfig
 
-if [ -e ~/.autoenv ]
-then
-    mv ~/.autoenv ~/.autoenv.old
-fi
+    if [ ! -e ~/dotfiles/.oh-my-zsh/custom ]; then
+        mkdir ~/dotfiles/.oh-my-zsh/custom
+    fi
+    ln -s ~/dotfiles/.oh-my-zsh/custom ~/.oh-my-zsh/custom
+}
 
-git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
-git clone https://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
-git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+function main() {
+    for item in ".oh-my-zsh .vim .zshrc .vimrc .autoenv"; do
+        save_to_old $item
+    done
 
-ln -s ~/dotfiles/.vimrc ~/.vimrc
-ln -s ~/dotfiles/.vim ~/.vim
-ln -s ~/dotfiles/.zshrc ~/.zshrc
-ln -s ~/dotfiles/.autoenv ~/.autoenv
-ln -s ~/dotfiles/.tmux.conf ~/.tmux.conf
-ln -s ~/dotfiles/.xinitrc ~/.xinitrc
-ln -s ~/dotfiles/.Xmodmap ~/.Xmodmap
+    do_clone
+    make_links
+}
 
-mkdir ~/dotfiles/.oh-my-zsh/custom
-ln -s ~/dotfiles/.oh-my-zsh/custom ~/.oh-my-zsh/custom
+main
 
 echo "Don't forget to run :PluginInstall! command in vim"
 
