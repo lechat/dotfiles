c337278777e1c24e7c0b93ed6d053aa268a9abad
Aleksey Maksimov <ctpeko3a@gmail.com>
2023-01-19 15:15:11 +0800

    Add vim-plug setup to setup

--- setup.sh
+++ setup.sh
@@ -12,9 +12,12 @@ function save_to_old() {
 }
 
 function do_clone() {
-    if [ ! -d ~/.vim/bundle/vundle/.git ]; then
-        git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
-    fi
+    curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+    sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+
     if [ ! -d ~/.oh-my-zsh/.git ]; then
     	git clone https://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
     fi
@@ -44,6 +47,10 @@ function make_links() {
     ln -sf ~/dotfiles/completions/_kubectx.zsh ~/.oh-my-zsh/completions/_kubectx.zsh
     ln -sf ~/dotfiles/local/bin/kubens ~/.local/bin/kubens
     ln -sf ~/dotfiles/completions/_kubens.zsh ~/.oh-my-zsh/completions/_kubens.zsh
+    if [ ! -d ~/.local/share/nvim ]; then
+        mkdir -p ~/.local/share/nvim
+        ln -sf ~/dotfiles/nvim/init.vim ~/.local/share/nvim/init.vim
+    fi
 }
 
 function main() {
